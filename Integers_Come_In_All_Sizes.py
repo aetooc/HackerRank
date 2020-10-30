f0cbbee46089d96629a49bfd3be3eed016a6a6fc
Muhammad Awais <72553466+m-awaisghani@users.noreply.github.com>
2020-10-30 20:09:40 +0500

    Integers_Come_In_All_Sizes.py
    Integers_Come_In_All_Sizes.py

---
+++ Integers_Come_In_All_Sizes.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print ((pow(a,b) + pow(c,d)))
